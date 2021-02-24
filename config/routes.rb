4d35d28222cca0ec8e5b1f1a124d01a378edf8b0
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-02-24 20:59:08 +0900

    Add post show delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations: 'registrations' }
 
-  # この行を編集する
   root 'posts#index'
 
   get '/users/:id', to: 'users#show', as: 'user'
 
-  resources :posts, only: %i(index new create) do
+  # この行を編集する
+  resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
   end
 end
